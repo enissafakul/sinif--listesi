70c9fe00b90d15d069a6e684b62b74adead4212a
enissafakul <messisafasuu@gmail.com>
2025-10-27 21:21:59 +0300

    Change sinif type from int to string

--- main (1).cpp
+++ main (1).cpp
@@ -16,7 +16,7 @@ public:
     string ad;
     string soyad;
     int yas;
-    int sinif;
+    string sinif;
 
     void bilgiGir(int i) {
         cout << "\n" << i + 1 << ". öğrencinin bilgilerini gir:\n";
